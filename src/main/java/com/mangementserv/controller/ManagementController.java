bf33950360605d5ca4f0088e95a37746de930272
dineshkrishnanj <dineshkrishnanj@arkainsure.com>
2020-06-22 19:45:31 +0530

    Conf file added

--- src/main/java/com/mangementserv/controller/ManagementController.java
+++ src/main/java/com/mangementserv/controller/ManagementController.java
@@ -1,6 +1,7 @@
 package com.mangementserv.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,8 @@ public class ManagementController {
 
     @PostMapping("/restart/service")
 
+    @GetMapping("/server")
+    public void getServer() {
+
+    }
 }
