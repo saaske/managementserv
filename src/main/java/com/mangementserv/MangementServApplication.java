5b1f1ae9a79a02c7100ff5c5e8c4621f0e373cba
sainathK <sainathk@gmail.com>
2020-06-22 19:31:21 +0530

    added

--- src/main/java/com/mangementserv/MangementServApplication.java
+++ src/main/java/com/mangementserv/MangementServApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MangementservApplication {
+public class MangementServApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MangementservApplication.class, args);
+		SpringApplication.run(MangementServApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/mangementserv/controller/ManagementController.java
@@ -0,0 +1,10 @@
+package com.mangementserv.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/management")
+public class ManagementController {
+
+}
