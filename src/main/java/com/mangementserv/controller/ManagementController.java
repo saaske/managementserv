4008ec3e449a43317dc462917593e82277369163
sainathK <sainathk@gmail.com>
2020-06-22 19:38:02 +0530

    vo files

--- src/main/java/com/mangementserv/controller/ManagementController.java
+++ src/main/java/com/mangementserv/controller/ManagementController.java
@@ -1,10 +1,13 @@
 package com.mangementserv.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/management")
 public class ManagementController {
+
+    @PostMapping("/restart/service")
 
 }
