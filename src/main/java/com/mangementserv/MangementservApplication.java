567c028e0bd9835088c3be7bf6f7c8ddc2834afd
sainathK <sainathk@gmail.com>
2020-06-22 19:18:57 +0530

    init commit

---
+++ src/main/java/com/mangementserv/MangementservApplication.java
@@ -0,0 +1,13 @@
+package com.mangementserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangementservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangementservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangementserv/MangementservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangementserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangementservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
